78930e095e16842e0c182bc406385ded38f576bd
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2024-03-19 15:01:42 +0700

    Change server code entirely

--- protobuf/src/main.rs
+++ protobuf/src/main.rs
@@ -1,83 +1,89 @@
-use protobuf_interface::{consensus_server::Consensus, Empty, Node};
 use tonic::{transport::Server, Request, Response, Status};
+use std::pin::Pin;
+use futures::StreamExt;
+use tokio::sync::Mutex;
+use std::sync::Arc;
+use crate::protobuf_interface::{
+    consensus_server::{Consensus, ConsensusServer},
+    *,
+};
+use std::collections::HashMap;
+use tokio_stream::wrappers::ReceiverStream;
 
-use crate::protobuf_interface::consensus_server::ConsensusServer;
-
-pub mod protobuf_interface {
-    tonic::include_proto!("protobuf_interface");
+pub struct SimpleConsensus {
+    nodes: Arc<Mutex<HashMap<Vec<u8>, Node>>>, // Using HashMap for easier node management based on public_key
 }
 
-#[derive(Debug, Default)]
-pub struct MyConsensus {}
-
 #[tonic::async_trait]
-impl Consensus for MyConsensus {
-    async fn clear_nodes(&self, _request: Request<Empty>) -> Result<Response<Empty>, Status> {
+impl Consensus for SimpleConsensus {
+    async fn clear_nodes(&self, request: Request<Empty>) -> Result<Response<Empty>, Status> {
+        let mut nodes = self.nodes.lock().await;
+        nodes.clear();
         Ok(Response::new(Empty {}))
     }
 
-    type EnumerateNodesStream = std::vec::IntoIter<Node>;
+    type EnumerateNodesStream = ReceiverStream<Result<Node, Status>>;
 
-    async fn enumerate_nodes(
-        &self,
-        _request: Request<Empty>,
-    ) -> Result<Response<Self::EnumerateNodesStream>, Status> {
-        let nodes = vec![Node {
-            public_key: vec![1, 2, 3],
-            address: "127.0.0.1".to_string(),
-            voting_power: 100,
-        }];
-        Ok(Response::new(nodes.into_iter()))
+    async fn enumerate_nodes(&self, request: Request<Empty>) -> Result<Response<Self::EnumerateNodesStream>, Status> {
+        let nodes = self.nodes.lock().await;
+        let (tx, rx) = tokio::sync::mpsc::channel(4);
+
+        for node in nodes.values() {
+            tx.send(Ok(node.clone())).await.unwrap();
+        }
+
+        Ok(Response::new(ReceiverStream::new(rx)))
     }
 
     async fn add_node(&self, request: Request<Node>) -> Result<Response<Empty>, Status> {
-        println!("Received request to add node: {:?}", request.get_ref());
+        let node = request.into_inner();
+        let mut nodes = self.nodes.lock().await;
+        nodes.insert(node.public_key.clone(), node);
         Ok(Response::new(Empty {}))
     }
 
-    async fn remove_node(&self, request: Request<Empty>) -> Result<Response<Empty>, Status> {
-        println!(
-            "Received request to remove node with public key: {:?}",
-            request.get_ref()
-        );
+    async fn remove_node(&self, request: Request<PublicKey>) -> Result<Response<Empty>, Status> {
+        let public_key = request.into_inner().data;
+        let mut nodes = self.nodes.lock().await;
+        nodes.remove(&public_key);
         Ok(Response::new(Empty {}))
     }
 
     async fn change_voting_power(&self, request: Request<Node>) -> Result<Response<Empty>, Status> {
-        println!(
-            "Received request to change voting power for node: {:?}",
-            request.get_ref()
-        );
+        // Simplified change voting power logic
+        let node_update = request.into_inner();
+        let mut nodes = self.nodes.lock().await;
+        if let Some(node) = nodes.get_mut(&node_update.public_key) {
+            node.voting_power = node_update.voting_power;
+        }
         Ok(Response::new(Empty {}))
     }
 
     async fn submit_block(&self, request: Request<Block>) -> Result<Response<Empty>, Status> {
-        println!(
-            "Received request to submit block with data: {:?}",
-            request.get_ref()
-        );
+        // Minimal logic for block submission
         Ok(Response::new(Empty {}))
     }
 
-    type GetBlockStreamStream = std::vec::IntoIter<Block>;
+    type GetBlockStreamStream = ReceiverStream<Result<Block, Status>>;
 
-    async fn get_block_stream(
-        &self,
-        _request: Request<Empty>,
-    ) -> Result<Response<Self::GetBlockStreamStream>, Status> {
-        let blocks = vec![Block {
-            data: vec![4, 5, 6],
-        }];
-        Ok(Response::new(blocks.into_iter()))
+    async fn get_block_stream(&self, request: Request<Empty>) -> Result<Response<Self::GetBlockStreamStream>, Status> {
+        let (tx, rx) = tokio::sync::mpsc::channel(4);
+
+        // Example: Sending an empty block for demonstration
+        tx.send(Ok(Block { data: vec![] })).await.unwrap();
+
+        Ok(Response::new(ReceiverStream::new(rx)))
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse().unwrap();
-    let consensus = MyConsensus::default();
+    let addr = "[::1]:50051".parse()?;
+    let consensus = SimpleConsensus {
+        nodes: Arc::new(Mutex::new(HashMap::new())),
+    };
 
-    println!("Consensus server running on {}", addr);
+    println!("Server listening on {}", addr);
 
     Server::builder()
         .add_service(ConsensusServer::new(consensus))
@@ -85,77 +91,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use protobuf_interface::consensus_client::ConsensusClient;
-    use tonic::transport::Channel;
-
-    async fn create_client() -> ConsensusClient<Channel> {
-        let addr = "[::1]:50051";
-        let channel = tonic::transport::Channel::from_shared(addr)
-            .unwrap()
-            .connect()
-            .await
-            .unwrap();
-        ConsensusClient::new(channel)
-    }
-
-    #[tokio::test]
-    async fn test_add_node() {
-        let client = create_client().await;
-        let request = tonic::Request::new(Node {
-            public_key: vec![1, 2, 3],
-            address: "127.0.0.1".to_string(),
-            voting_power: 100,
-        });
-
-        let response = client.add_node(request).await.unwrap();
-        assert_eq!(response.get_ref(), &Empty {});
-    }
-
-    #[tokio::test]
-    async fn test_remove_node() {
-        let client = create_client().await;
-        let request = tonic::Request::new(Empty {});
-
-        let response = client.remove_node(request).await.unwrap();
-        assert_eq!(response.get_ref(), &Empty {});
-    }
-
-    #[tokio::test]
-    async fn test_change_voting_power() {
-        let client = create_client().await;
-        let request = tonic::Request::new(Node {
-            public_key: vec![1, 2, 3],
-            address: "127.0.0.1".to_string(),
-            voting_power: 200,
-        });
-
-        let response = client.change_voting_power(request).await.unwrap();
-        assert_eq!(response.get_ref(), &Empty {});
-    }
-
-    #[tokio::test]
-    async fn test_submit_block() {
-        let client = create_client().await;
-        let request = tonic::Request::new(Block {
-            data: vec![4, 5, 6],
-        });
-
-        let response = client.submit_block(request).await.unwrap();
-        assert_eq!(response.get_ref(), &Empty {});
-    }
-
-    #[tokio::test]
-    async fn test_get_block_stream() {
-        let client = create_client().await;
-        let request = tonic::Request::new(Empty {});
-
-        let mut stream = client.get_block_stream(request).await.unwrap().into_inner();
-        let block = stream.next().await.unwrap().unwrap();
-        assert_eq!(block.data, vec![4, 5, 6]);
-    }
 }
